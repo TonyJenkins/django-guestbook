bc76ffff5d63a628ffe3a39a8f8abde36be6f9b2
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2017-08-29 15:06:42 +0100

    Project setup. Route and template.

--- guestbook/guestbook/settings.py
+++ guestbook/guestbook/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Project Settings
+
+TEMPLATE_DIR = os.path.join (BASE_DIR, 'templates')
+STATIC_DIR = os.path.join (BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'guestbook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
@@ -119,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
+
+
--- guestbook/guestbookapp/views.py
+++ guestbook/guestbookapp/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index (request):
+
+    context = {}
+
+    response = render (request, 'guestbookapp/index.html', context)
+    return response
