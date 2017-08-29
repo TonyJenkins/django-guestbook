956e6c058e6bf4adfb9276f6525e97442e21f2dd
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2017-08-29 15:35:21 +0100

    Initial model.

--- guestbook/guestbookapp/models.py
+++ guestbook/guestbookapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Entry (models.Model):
+
+    user = models.CharField (max_length = 64)
+    comment = models.CharField (max_length = 128)
+
+    class Meta:
+        verbose_name_plural = 'Entries'
+
+    def __str__ (self):
+        return self.user + ' : ' + self.comment
