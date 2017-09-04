37f26de4a46f7e059e365f5f8c42aa2d2371a69d
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2017-09-04 12:10:28 +0100

    Display complete.

--- guestbook/guestbookapp/views.py
+++ guestbook/guestbookapp/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from guestbookapp.models import Entry
 
 def index (request):
 
     context = {}
+
+    entry_list = Entry.objects.all ()
+    context ['entries'] = entry_list
 
     response = render (request, 'guestbookapp/index.html', context)
     return response
