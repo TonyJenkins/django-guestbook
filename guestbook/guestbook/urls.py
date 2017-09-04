13c5a18ac584ebaabb8c4748ab01bf1b3f96b64f
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2017-09-04 15:35:09 +0100

    Add URL mapping to root of site.

--- guestbook/guestbook/urls.py
+++ guestbook/guestbook/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from guestbookapp import views
+from guestbookapp.views import index
 
 urlpatterns = [
     url (r'^admin/', admin.site.urls),
+    url (r'^$', index),
     url (r'^guestbook/', include ('guestbookapp.urls')),
 ]
